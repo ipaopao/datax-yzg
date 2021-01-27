be4212dd9c267edd2f0a9e48d57f3835f60cd1f4
ipaopao <yangzhiguo@360.com>
2021-01-27 11:58:20 +0800

    demo1

---
+++ mysqlreader/src/main/java/com/alibaba/datax/plugin/reader/mysqlreader/demo1.java
@@ -0,0 +1,16 @@
+package com.alibaba.datax.plugin.reader.mysqlreader;
+
+/**
+ * @ClassName demo1
+ * @Description TODO
+ * @Author yangzhiguo
+ * @Date 2021/1/27 11:57
+ * @Version 1.0
+ **/
+
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
